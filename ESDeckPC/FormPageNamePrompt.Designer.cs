755a2d6052c701b8541d3420f5e97a29ed72a28c
HankWang <hank.wang@koso.com.tw>
2026-06-30 15:04:56 +0800

    新增PageName的編輯視窗

---
+++ ESDeckPC/FormPageNamePrompt.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ESDeckPC
+{
+    partial class FormTabNamePrompt
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            // lblTitle
+            //
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.lblTitle.Location = new System.Drawing.Point(12, 14);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(75, 14);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Page name:";
+            //
+            // txtName
+            //
+            this.txtName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.txtName.Location = new System.Drawing.Point(12, 36);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(256, 22);
+            this.txtName.TabIndex = 1;
+            //
+            // btnOk
+            //
+            this.btnOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(58)))));
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOk.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.btnOk.Location = new System.Drawing.Point(112, 72);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(76, 26);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = false;
+            this.btnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            //
+            // btnCancel
+            //
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(58)))));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.btnCancel.Location = new System.Drawing.Point(192, 72);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(76, 26);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            //
+            // FormTabNamePrompt
+            //
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(280, 110);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Consolas", 9F);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormTabNamePrompt";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FormTabNamePrompt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ ESDeckPC/FormPageNamePrompt.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ESDeckPC
+{
+    /// <summary>
+    /// Small modal dialog used for both "new page" and "rename page".
+    /// Returns the entered name via NameValue when DialogResult == OK.
+    /// All controls are defined in FormTabNamePrompt_Designer.cs.
+    /// </summary>
+    public partial class FormTabNamePrompt : Form
+    {
+        public string NameValue => txtName.Text.Trim();
+
+        [System.Runtime.InteropServices.DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr,
+                                                        ref int attrValue, int attrSize);
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            int v = 1;
+            DwmSetWindowAttribute(this.Handle, DWMWA_USE_IMMERSIVE_DARK_MODE, ref v, sizeof(int));
+        }
+
+        public FormTabNamePrompt(string title, string initialValue)
+        {
+            InitializeComponent();
+            Text = title;
+            txtName.Text = initialValue ?? "";
+            txtName.SelectAll();
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                DialogResult = DialogResult.None;
+                MessageBox.Show(this, "Name cannot be empty.", "Invalid name",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /// <summary>
+        /// Convenience helper: show the dialog and return the entered name,
+        /// or null if the user cancelled.
+        /// </summary>
+        public static string Show(IWin32Window owner, string title, string initialValue = "")
+        {
+            using (var dlg = new FormTabNamePrompt(title, initialValue))
+            {
+                return dlg.ShowDialog(owner) == DialogResult.OK ? dlg.NameValue : null;
+            }
+        }
+    }
+}
